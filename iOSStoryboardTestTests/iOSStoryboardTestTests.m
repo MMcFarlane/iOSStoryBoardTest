94d6bc44cff8b61be13075e7a3e38aff1eaf92ba
Maurice McFarlane <mscmcfarlane26@gmail.com>
2014-02-03 21:19:06 -0500

    Initial Commit from LocalRepo

---
+++ iOSStoryboardTestTests/iOSStoryboardTestTests.m
@@ -0,0 +1,34 @@
+//
+//  iOSStoryboardTestTests.m
+//  iOSStoryboardTestTests
+//
+//  Created by Maurice McFarlane on 2/3/14.
+//  Copyright (c) 2014 MMcFarlane. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSStoryboardTestTests : XCTestCase
+
+@end
+
+@implementation iOSStoryboardTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
